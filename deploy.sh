c7912293eece66d9c51afd8d0fc1a1905e87950a
Diego Versiani <diegoversiani@gmail.com>
2017-05-05 14:16:44 +0200

    Change ssh-add command for travis integration.

--- deploy.sh
+++ deploy.sh
@@ -50,14 +50,10 @@ git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_c47d449edf81_key"
-ENCRYPTED_IV_VAR="encrypted_c47d449edf81_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $encrypted_c47d449edf81_key -iv $encrypted_c47d449edf81_iv -in ../deploy_key.enc -out ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
